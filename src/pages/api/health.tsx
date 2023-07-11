751a6751c6da99310087c4095f013445a073f41c
Jared Johnson <jared@jaredjohnson.dev>
2023-07-10 20:05:21 -0400

    Add /api/health handler

---
+++ src/pages/api/health.tsx
@@ -0,0 +1,12 @@
+import { METHOD_NOT_ALLOWED, OK } from "@/lib/http/codes";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  if (req.method !== "GET") {
+    res.status(METHOD_NOT_ALLOWED);
+  }
+  res.status(OK).send("ok");
+}
