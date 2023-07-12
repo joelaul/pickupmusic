21b8b121e2cf42a2b8a5fd16af2cfbb9a14c6986
Jared Johnson <jared@jaredjohnson.dev>
2023-07-12 15:21:52 -0400

    Setup layout

---
+++ src/components/nav/menu.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+export type MenuProps = {
+  open: boolean;
+};
+
+export const Menu: React.FC<MenuProps> = (props) => {
+  const menuTransitionClassNames = props.open
+    ? "transition ease-out duration-100"
+    : "transition ease-in duration-75";
+  const menuTransformClassNames = props.open
+    ? "transform opacity-100 scale-100"
+    : "transform opacity-0 scale-95";
+  const menuClassNames = `${menuTransitionClassNames} ${menuTransformClassNames}`;
+
+  return (
+    <div
+      className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${menuClassNames}`}
+      role="menu"
+      aria-orientation="vertical"
+      aria-labelledby="user-menu-button"
+    >
+      <a
+        href="#"
+        className="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabIndex={-1}
+        id="user-menu-item-0"
+      >
+        Your Profile
+      </a>
+      <a
+        href="#"
+        className="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabIndex={-1}
+        id="user-menu-item-1"
+      >
+        Settings
+      </a>
+      <a
+        href="#"
+        className="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabIndex={-1}
+        id="user-menu-item-2"
+      >
+        Sign out
+      </a>
+    </div>
+  );
+};
