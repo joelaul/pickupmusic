f5177ddccfd146c6bec61adab5a14612328655ce
Jared Johnson <jared@jaredjohnson.dev>
2023-07-15 11:44:01 -0400

    Remove /search link from menu

--- src/components/Nav/constants.ts
+++ src/components/Nav/constants.ts
@@ -3,7 +3,6 @@ import { NavItem } from '@/components/Nav/types';
 export const MENU_ITEMS: NavItem[] = [
   { title: 'Home', href: '/' },
   { title: 'Classes', href: '/classes' },
-  { title: 'Search', href: '/search' },
 ];
 
 export const PROFILE_DROPDOWN_ITEMS: NavItem[] = [
