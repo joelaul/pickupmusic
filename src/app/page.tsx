544eeb4f774042707260089783d7aa091dfd13c5
Jared Johnson <jared@jaredjohnson.dev>
2023-07-16 16:22:52 -0400

    Add background css to home image and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,38 @@
 import Badges from '@/components/Badges/Badges';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (
     <main>
-      <div className="py-24 sm:py-32">
+      <div
+        className="py-24 sm:py-32"
+        style={{
+          backgroundImage: 'url(splash.jpg)',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          boxShadow: 'inset 0 0 0 2000px rgba(0, 0, 0, 0.6)',
+        }}
+      >
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl">
+          <div className="text-left mx-auto max-w-2xl">
             <h2 className="text-base font-semibold leading-7 text-indigo-600">
               NEW RELEASE
             </h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-50 sm:text-4xl">
               Groove Learning Pathway
             </p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-gray-100">
               Learn how to sit in the pocket and funk it up.
             </p>
+            <div className="mt-10 flex items-center justify-items-start gap-x-6">
+              <a
+                href="/classes/advanced-fusion-and-funk-guitar/part1-fundamentals-rhythm"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Learn now
+              </a>
+            </div>
           </div>
         </div>
       </div>
