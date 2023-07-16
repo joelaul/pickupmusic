395f459525f069894eb2a29ab301723f89b8ff11
Jared Johnson <jared@jaredjohnson.dev>
2023-07-16 16:30:22 -0400

    Change nav color to fit theme

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -16,14 +16,14 @@ export const Nav = () => {
   }
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-indigo-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -52,8 +52,8 @@ export const Nav = () => {
                         href={href}
                         className={classNames(
                           isCurrent(href)
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            ? 'bg-indigo-900 text-white'
+                            : 'text-gray-300 hover:bg-indigo-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium',
                         )}
                         aria-current={isCurrent(href) ? 'page' : undefined}
@@ -68,7 +68,7 @@ export const Nav = () => {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="flex rounded-full bg-indigo-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
                       <Image
                         className="h-8 w-8 rounded-full"
@@ -94,7 +94,7 @@ export const Nav = () => {
                           <a
                             href="/settings"
                             className={classNames(
-                              active ? 'bg-gray-100' : '',
+                              active ? 'bg-indigo-100' : '',
                               'block px-4 py-2 text-sm text-gray-700',
                             )}
                           >
@@ -118,8 +118,8 @@ export const Nav = () => {
                   href={href}
                   className={classNames(
                     isCurrent(href)
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      ? 'bg-indigo-900 text-white'
+                      : 'text-gray-300 hover:bg-indigo-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium',
                   )}
                   aria-current={isCurrent(href) ? 'page' : undefined}
