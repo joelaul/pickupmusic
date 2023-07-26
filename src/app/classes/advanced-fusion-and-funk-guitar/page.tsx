67f3f766656a2da4f36befae81832d85306c8702
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 16:11:24 -0400

    Remove class part routes and add todo class

--- src/app/classes/advanced-fusion-and-funk-guitar/page.tsx
+++
@@ -1,5 +0,0 @@
-// For https://app.pickupmusic.com/products/advanced-fusion-and-funk-guitar
-
-export default function ClassLanding() {
-  return <div>ClassLanding</div>;
-}
---
+++ src/app/classes/todo/page.tsx
@@ -0,0 +1,3 @@
+export default function Klass() {
+  return <div>Class</div>;
+}
