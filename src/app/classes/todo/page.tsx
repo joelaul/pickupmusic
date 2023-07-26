5b5fba199adca00c200f696d752a2d4526b1367d
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 16:22:57 -0400

    Flesh out lorem ipsum copy on TODO class

--- src/app/classes/todo/page.tsx
+++ src/app/classes/todo/page.tsx
@@ -3,6 +3,12 @@ import {
   LockClosedIcon,
   ServerIcon,
 } from '@heroicons/react/20/solid';
+import {
+  ArrowsPointingOutIcon,
+  CurrencyDollarIcon,
+  ExclamationCircleIcon,
+  MusicalNoteIcon,
+} from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
 export default function Class() {
@@ -44,10 +50,10 @@ export default function Class() {
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
               <p className="text-base font-semibold leading-7 text-indigo-600">
-                Deploy faster
+                Break out of your box
               </p>
               <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                A better workflow
+                Using the CAGED system
               </h1>
               <p className="mt-6 text-xl leading-8 text-gray-700">
                 Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
@@ -79,13 +85,13 @@ export default function Class() {
               </p>
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
-                  <CloudArrowUpIcon
+                  <MusicalNoteIcon
                     className="mt-1 h-5 w-5 flex-none text-indigo-600"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Push to deploy.
+                      Play anywhere.
                     </strong>{' '}
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Maiores impedit perferendis suscipit eaque, iste dolor
@@ -93,26 +99,26 @@ export default function Class() {
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  <LockClosedIcon
+                  <ExclamationCircleIcon
                     className="mt-1 h-5 w-5 flex-none text-indigo-600"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      SSL certificates.
+                      Seriously. Anywhere.
                     </strong>{' '}
                     Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                     qui lorem cupidatat commodo.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  <ServerIcon
+                  <CurrencyDollarIcon
                     className="mt-1 h-5 w-5 flex-none text-indigo-600"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Database backups.
+                      Become rich with the CAGED system.
                     </strong>{' '}
                     Ac tincidunt sapien vehicula erat auctor pellentesque
                     rhoncus. Et magna sit morbi lobortis.
@@ -127,8 +133,9 @@ export default function Class() {
                 et ut. Ac lorem vel integer orci.
               </p>
               <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                No server? No problem.
+                Do you enjoy lorem ipsum soup?
               </h2>
+              <h3>Here's some more.</h3>
               <p className="mt-6">
                 Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
                 consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
