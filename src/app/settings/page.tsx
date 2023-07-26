e346aac9b89f832e8734846ad9cc8426cc7a9acf
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 16:42:40 -0400

    Add Not Available page for settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,12 +2,12 @@ export default function Settings() {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
+        <p className="text-base font-semibold text-indigo-600">503</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Under Construction
+          Not Available
         </h1>
         <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, the settings page is not available.
+          Sorry, the settings page is not available in the pickupmusic demo.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
