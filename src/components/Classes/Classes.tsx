8059c928b21b9460e48db93b99006b358604f387
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 15:34:10 -0400

    Render basic cards from https://tailwindcomponents.com/component/responsive-team-cards-2/landing

--- src/components/Classes/Classes.tsx
+++ src/components/Classes/Classes.tsx
@@ -1,4 +1,21 @@
+import { Cards } from '@/components/Cards/Cards';
+
 // TODO(jared): Only show a few classes?
 export default function Classes() {
-  return <div>Classes</div>;
+  return (
+    <>
+      <br />
+
+      <div className="text-center pb-12">
+        <h2 className="text-base font-bold text-indigo-600">
+          We have the best equipment in the market
+        </h2>
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+          Check our awesome team members
+        </h1>
+      </div>
+
+      <Cards cards={[]} />
+    </>
+  );
 }
