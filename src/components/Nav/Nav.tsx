657d14ef1ba964dbd4a035ecf7db68ee76227e39
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 15:15:30 -0400

    Reduce the recent badge cols from 5 to 3

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -78,7 +78,7 @@ export const Nav = () => {
               {/* Nav - right side */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Badge preview */}
-                <div className="h-6 grid grid-cols-5 gap-1 hover:scale-110   hover:cursor-pointer">
+                <div className="h-6 grid grid-cols-3 gap-1 hover:scale-110   hover:cursor-pointer">
                   {BADGES_ACQUIRED(BADGES)
                     .slice(-3)
                     .map(({ imgSrc }) => (
