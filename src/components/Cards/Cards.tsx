43efc609c4c325de5b2109315881682472f033ff
Jared Johnson <jared@jaredjohnson.dev>
2023-07-26 15:49:28 -0400

    Use next <Image /> instead of plain <img /> in the Cards component

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,5 +1,6 @@
 import { Card } from '@/lib/card/types';
 import React from 'react';
+import Image from 'next/image';
 
 export type CardProps = {
   cards: Card[];
@@ -9,21 +10,23 @@ export const Cards: React.FC<CardProps> = () => {
   return (
     <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="border-2 border-gray-100 w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-          <div>
-            <img
-              className="object-center object-cover h-auto w-full"
-              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-              alt="photo"
-            />
+        <a href="TODO">
+          <div className="border-2 border-gray-100 w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+            <div>
+              <Image
+                alt="groove"
+                className="object-center object-cover h-auto w-full"
+                src="/cards/groove.png"
+                width={2700}
+                height={1900}
+              />
+            </div>
+            <div className="text-center py-8 sm:py-6">
+              <p className="text-xl text-gray-700 font-bold mb-2">GROOVE</p>
+              <p className="text-base text-gray-500 font-normal">Kelyn Crapp</p>
+            </div>
           </div>
-          <div className="text-center py-8 sm:py-6">
-            <p className="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
-            <p className="text-base text-gray-500 font-normal">
-              Software Engineer
-            </p>
-          </div>
-        </div>
+        </a>
       </div>
     </section>
   );
